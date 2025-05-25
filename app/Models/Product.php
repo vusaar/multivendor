148ef05d07648820cec165ffd4d39c5f44795086
vusaar <sourcecode@nust.ac.zw>
2025-05-25 12:49:32 +0200

    created basic model classes, vendor, product category

---
+++ app/Models/Product.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'vendor_id',
+        'category_id',
+        'name',
+        'description',
+        'price',
+        'stock',
+        'image',
+        'status',
+    ];
+
+    public function vendor()
+    {
+        return $this->belongsTo(Vendor::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function images()
+    {
+        return $this->hasMany(ProductImage::class);
+    }
+}
